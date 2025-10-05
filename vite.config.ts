097087aeb654aabda7dbb2006021b3fb65c8ba97
Rustam <baziev.rustam1@gmail.com>
2025-10-05 13:13:08 +0200

    /

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Vite просто обрабатывает CSS, Tailwind через PostCSS встроено
 export default defineConfig({
   plugins: [react()],
 });
